ed020f756fffe209aec9cfdf4b808c61df32b70d
Edgar Fernando García de León <101934110+egarciad19@users.noreply.github.com>
2024-05-29 14:20:11 -0600

    Update conf.js
    se modifca contraseña

--- Configs/conf.js
+++ Configs/conf.js
@@ -2,7 +2,7 @@ module.exports={
 
     host :"us-cluster-east-01.k8s.cleardb.net",
     user:"be32af4f4a6ca8",
-    password:"e7a69505",
+    password:"f12970fb1c7feeb",
     db:"heroku_aa513e4da328873",
     port: 3306
 }
