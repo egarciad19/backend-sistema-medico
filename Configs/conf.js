ae58a75edcf05d9a1ecf8c6c7b3aafd3260b1e61
Edgar Fernando García de León <101934110+egarciad19@users.noreply.github.com>
2024-05-28 23:15:14 -0600

    Update conf.js

--- Configs/conf.js
+++ Configs/conf.js
@@ -2,7 +2,7 @@ module.exports={
 
     host :"us-cluster-east-01.k8s.cleardb.net",
     user:"be32af4f4a6ca8",
-    password:"admin147100",
+    password:"e7a69505",
     db:"heroku_aa513e4da328873",
     port: 3306
 }
